6965597373eb8841efae5e3477d825a0f3ec87ce
MrThuy <mrthuy@hotmail.com>
2018-08-22 23:06:43 -0300

    CheckGame

--- js/script.js
+++ js/script.js
@@ -1,48 +1,106 @@
 var player;
+var endGame;
 
-function novoJogo(){
-    $('#t1 td').html('');
-    player = 'X';
+function novoJogo() {
+  $('#t1 td').html('');
+  $('#t1 tr').removeClass();
+  $('#t1 tr td').removeClass();
+  player = 'X';
+  endGame = false;
 }
 
-function checkGame(){
-    console.log('checkGame');
-}
+function checkGame() {
 
-function jogada(){
-    if( $(this).html() == ''){
-        $(this).html(player);
-        player = player == 'X'? 'O' : 'X';
-        console.log('jogada');
-        checkGame();
+  $('#t1 tr').each(function () {
+    //Verifica linhas
+    let val = $(this).children('td:eq(0)').html();
+    if ((val != '') &&      
+      ($(this).children('td:eq(1)').html() == val) &&
+      ($(this).children('td:eq(2)').html() == val)) {
+      $(this).addClass('bg-success');
+      endGame = true;
     }
+
+    //Verifica colunas
+    let col;
+    for (col = 0; col < 3; col++) {
+      let val = $('#t1 tr:eq(0) td:eq(' + col + ')').html();      
+      if ((val != '') &&        
+          ($('#t1 tr:eq(1) td:eq(' + col + ')').html() == val) &&
+          ($('#t1 tr:eq(2) td:eq(' + col + ')').html() == val)) {
+
+        $('#t1 tr:eq(0) td:eq(' + col + '), '+
+          '#t1 tr:eq(1) td:eq(' + col + '), '+
+          '#t1 tr:eq(2) td:eq(' + col + ')').addClass('bg-success');
+        endGame = true;
+      }
+    }
+
+    //Verifica diagonal 1
+    let valD1 = $('#t1 tr:eq(0) td:eq(0)').html();   
+    if( (valD1 != '') &&        
+        ($('#t1 tr:eq(1) td:eq(1)').html() == valD1) &&
+        ($('#t1 tr:eq(2) td:eq(2)').html() == valD1) ) {
+
+      $('#t1 tr:eq(0) td:eq(0), '+
+        '#t1 tr:eq(1) td:eq(1), '+
+        '#t1 tr:eq(2) td:eq(2)').addClass('bg-success');
+      endGame = true;
+    }
+
+    //Verifica diagonal 2
+    let valD2 = $('#t1 tr:eq(0) td:eq(2)').html();   
+    if( (valD2 != '') &&        
+        ($('#t1 tr:eq(1) td:eq(1)').html() == valD2) &&
+        ($('#t1 tr:eq(2) td:eq(0)').html() == valD2) ) {
+
+      $('#t1 tr:eq(0) td:eq(2), '+
+        '#t1 tr:eq(1) td:eq(1), '+
+        '#t1 tr:eq(2) td:eq(0)').addClass('bg-success');
+      endGame = true;
+    }
+
+  });
 }
 
-$(document).ready(function(){
-    
-    novoJogo();
-    
-    $('#newGame').click(novoJogo);
+function jogada() {
+  if ( ($(this).html() == '') &&
+       (!endGame) ) {
+    $(this).html(player);
+    player = player == 'X' ? 'O' : 'X';
+    checkGame($(this));
+  }
+}
 
-    $('#t1 td').click(jogada);
+$(document).ready(function () {
 
-    $('td').hover( function(){
-        if( $(this).html() == ''){
-            $(this).css({
-                "background-color" : "rgb(26, 139, 26, 0.8)",
-                "cursor" : "crosshair"
-            });        
-        }else{
-            $(this).css({
-                "background-color" : "rgb(214, 55, 55, 0.8)",
-                "cursor":"not-allowed"
-        });        
-        }
-    }, function(){ 
+  novoJogo();
+
+  $('#newGame').click(novoJogo);
+
+  $('#t1 td').click(jogada);
+
+  $('td').hover(function () {
+    if (!endGame) {
+      if ($(this).html() == '') {
         $(this).css({
-            "background-color": "white",
-            "cursor" : "default"
-        }); 
+          "background-color": "#28a745",
+          "cursor": "crosshair"
+        });
+      } else {
+        $(this).css({
+          "background-color": "#dc3545",
+          "cursor": "not-allowed"
+        });
+      }
+    }
+  }, function () {
+
+    $(this).css({
+      "background-color": "",
+      "cursor": ""
     });
+
+  });
 
 });
